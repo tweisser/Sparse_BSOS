0e64761bb7176ff0d9a891c48db30ea4b027ac4d
Tillmann Weisser <tweisser@laas.fr>
2017-05-19 12:35:41 +0200

    Authors and information added

---
+++ _certificates/aux/gendata2.m
@@ -0,0 +1,89 @@
+%********************************************************************
+%% gendata2: creates different SDP relaxation to a given POP
+%%
+%% pop: structure with fields F,G,d,(k),(I),(J),n
+%% algotag: specifies relaxation ['SBSOS'|'BSOS'|'SPUT']
+%% solvertag: specifies solver ['sdpt3'|'sqlp'|'sedumi'] (optional)
+%%
+%% Author: Tillmann Weisser
+%%********************************************************************
+
+function sdp = gendata2(pop,algotag,solvertag)
+if nargin < 3
+    convert2solvertag = 0;
+else
+    convert2solvertag = 1;
+end
+
+if strcmp(algotag,'BSOS')
+    tstart=clock;
+    fprintf('\n This is BSOS')
+    [blk,At,C,b,xrand,maxdeg] = genSDPdata(pop.F,pop.G,pop.d,pop.k);
+    tGEN = etime(clock,tstart);
+    sdp.id = 'BSOS';
+    sdp.blk = blk;
+    sdp.At = At;
+    sdp.C = C;
+    sdp.b = b;
+    sdp.tGEN = tGEN;
+    sdp.xrand = xrand;
+    sdp.maxdeg = maxdeg;
+    sdp.d = pop.d;
+    sdp.k = pop.k;
+    sdp.n = size(pop.F,2)-1;
+elseif strcmp(algotag,'SBSOS')
+    tstart=clock;
+    [blk,At,C,b,constant,time,recy,degfg] = genSBSOSdata(pop.F,pop.G,pop.I,pop.J,pop.d,pop.k);
+    tGEN = etime(clock,tstart);
+    sdp.id = 'SBSOS';
+    sdp.blk = blk;
+    sdp.At = At;
+    sdp.C = C;
+    sdp.b = b;
+    sdp.tGEN = tGEN;
+    sdp.constant = constant;
+    sdp.recy = recy;
+    sdp.degfg = degfg;
+    sdp.d = pop.d;
+    sdp.k = pop.k;
+    sdp.n = size(pop.F,2)-1;
+    
+elseif strcmp(algotag,'SPUT')
+    tstart=clock;
+    [blk,At,C,b,constant,time,recy,degfg] = genSPUTdata(pop.F,pop.G,pop.I,pop.J,pop.d);
+    tGEN = etime(clock,tstart);
+    sdp.id = 'SPUT';
+    sdp.blk = blk;
+    sdp.At = At;
+    sdp.C = C;
+    sdp.b = b;
+    sdp.tGEN = tGEN;
+    sdp.constant = constant;
+    sdp.recy = recy;
+    sdp.degfg = degfg;
+    sdp.d = pop.d;
+    sdp.n = size(pop.F,2)-1;
+else
+    error('use algotag BSOS, SBSOS or SPUT')
+end
+
+if convert2solvertag&&strcmp(solvertag,'sedumi')
+    fprintf('\n Converting to SeDuMi format... \n')
+    tstart=clock;
+    [At,b,c,K] = sdpt3tosedumi(blk,At,C,b);
+    tGEN = tGEN + etime(clock,tstart);
+    recy.converted = 'sedumi';
+    sdp.blk = K;
+    sdp.At = At;
+    sdp.C = c;
+    sdp.b = b;
+    sdp.tGEN = tGEN;
+    sdp.recy = recy;
+    if strcmp(algotag,'BSOS')
+        sdp.pars.sdp = 0;
+    elseif length(pop.I)==1
+        sdp.pars.sdp = 0;
+    else
+        sdp.pars.sdp = 1;
+    end
+end
---
+++ _certificates/aux/postproc.m
@@ -0,0 +1,76 @@
+%%***************************************************************%%
+%% postproc: evaluates solution of an SDP created by gendata2 and 
+%%	     solved by csol
+%% pop: structure with fields F,G,d,(k),(I),(J),n
+%% sdp: structure created by gendata2
+%% sol: structure created by csol
+%%
+%% Author: Tillmann Weisser
+%%***************************************************************%%
+
+    function psol = postproc(pop,sdp,sol)
+
+    if strcmp(sdp.id,'BSOS')
+        tstart = clock;
+        psol.obj = (-sol.obj(1));
+        [psol.rnk, psol.mineig] = rankBSOS(sdp.n,sdp.k,sol.y,sdp.xrand);
+        tval = etime(clock,tstart);
+        psol.obj = (-sol.obj(1));
+    elseif strcmp(sdp.id,'SBSOS')
+        tstart = clock;
+        psol.obj = (-sol.obj(1))+sdp.constant;
+        YY = recovery(sdp.recy,sol.y);
+        if 1
+            [RANK,mineig] = rankSBSOS(YY,sdp.recy.I,sdp.k);
+            psol.rnk = sum(RANK)/length(RANK);
+            psol.mineig = mineig;
+            psol.YY = YY;
+            if (psol.rnk ==1)&&(mineig > -10^6)
+                psol.xsol = mom2x(YY,pop);
+                ccontrol=zeros(length(pop.G),1);
+                for j = 1:length(pop.G)
+                    ccontrol(j) = evalpoly(pop.G{j},psol.xsol);
+                end
+                if any(ccontrol<-10^-6)||any(ccontrol>1+10^-6)
+                    fprintf('Check constraints control: %d constraints might be violated.\n',sum(ccontrol<-10^-6)+sum(ccontrol>1+10^-6));
+                end
+                psol.fx=evalpoly(pop.F,psol.xsol);
+                psol.ccontrol = ccontrol;
+                fprintf('\n average rank M(y) = %2.1f \n minimal eigenvalue = %2.4e \n',psol.rnk,mineig)
+                fprintf('\n optimal solution extracted: \n f(x) = %2.4e \n', full(psol.fx))
+            end
+        end
+        tval = etime(clock,tstart);
+        psol.obj = (-sol.obj(1))+sdp.constant;
+    elseif strcmp(sdp.id,'SPUT')
+        tstart = clock;
+        psol.obj = (-sol.obj(1))+sdp.constant;
+        YY = recovery(sdp.recy,sol.y);
+        [RANK,mineig] = rankSPUT(YY,sdp.recy.I,sdp.degfg,pop.d);
+        psol.rnk = sum(RANK)/length(RANK);
+        psol.mineig = mineig;
+        psol.YY = YY;
+        if psol.rnk ==1
+            psol.xsol = mom2x(YY,pop);
+            ccontrol=zeros(length(pop.G),1);
+            for j = 1:length(pop.G)
+                ccontrol(j) = evalpoly(pop.G{j},psol.xsol);
+            end
+            if any(ccontrol<-10^-6)||any(ccontrol>1+10^-6)
+                fprintf('Check constraints control: %d constraints might be violated.\n',sum(ccontrol<-10^-6)+sum(ccontrol>1+10^-6));
+            end
+            psol.fx=evalpoly(pop.F,psol.xsol);
+            psol.ccontrol = ccontrol;
+            fprintf('\n average rank M(y) = %2.1f \n minimal eigenvalue = %2.4e \n',psol.rnk,mineig)
+            fprintf('\n optimal solution extracted: \n f(x) = %2.4e \n', psol.fx)
+        end
+        tval = etime(clock,tstart);
+    else
+        error('can not postproc this algotag')
+    end
+
+    psol.ttot = sdp.tGEN + sol.tSOL + tval;
+    fprintf('\n primal objective value: %2.4e \n', psol.obj)
+
+
+    end
